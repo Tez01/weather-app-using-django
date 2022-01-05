fb57e6d8d4dd4808c7f7b53dddeeba8a4081215c
Tej <tejpartap02@gmail.com>
2022-01-05 10:52:29 -0500

    Get api request from allweather.org to get required parameters

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        city = request.POST['city']
+        response = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=eca532a0962ecd1dd72b64d1d2084f6b').read()
+        json_data = json.loads(response)
+        data = {
+            'country_code': str(json_data['sys']['country']),
+            'coordinate': str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+            'temp' : str(json_data['main']['temp']) + 'k',
+            'pressure': str(json_data['main']['pressure']),
+            'humidity': str(json_data['main']['humidity']),
+        }
+
+
+    else:
+        city = ''
+        data = {}
+    return render(request, 'index.html',  {'city': city, 'data': data})
